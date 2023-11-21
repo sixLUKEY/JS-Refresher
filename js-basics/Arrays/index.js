93c41f3c94e12db7910c67a6675e80f287e536e4
sixLUKEY <marcoslukerojo@gmail.com>
2023-11-21 15:35:56 +0200

    Arrays

---
+++ js-basics/Arrays/index.js
@@ -0,0 +1,11 @@
+let selectedColors = ['red', 'blue']
+console.log( selectedColors )
+console.log( selectedColors[0] )
+
+selectedColors[2] = 'green'
+console.log( selectedColors )
+
+selectedColors[3] = 5
+console.log( selectedColors )
+
+console.log( selectedColors.length )
