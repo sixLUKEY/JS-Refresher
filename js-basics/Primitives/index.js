0ff009b91bdc1994e00f275236939b608ed24d44
sixLUKEY <marcoslukerojo@gmail.com>
2023-11-21 15:18:02 +0200

    primitives

---
+++ js-basics/Primitives/index.js
@@ -0,0 +1,7 @@
+let name = "Luke" //String Literal
+let age = 22 //Number Literal
+let isApproved = true //Boolean Literal
+let firstName = undefined
+let lastName = null
+let selectedColor = 'red';
+ selectedColor = null //Clear the value of a variable
