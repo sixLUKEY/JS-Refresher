e2a8674f3563f4de2f7543bf5463deeec09f0e41
sixLUKEY <marcoslukerojo@gmail.com>
2023-11-21 15:05:34 +0200

    variables complete

---
+++ js-basics/index.js
@@ -0,0 +1,14 @@
+//comments
+console.log("hello World");
+
+// let name = 'Luke';
+// console.log(name);
+
+//Make sure variables are not reserved keywords
+//Use meaningful and descriptive names
+//Cannot start variable with a number
+//Cannot contain a space or hyphen
+
+let firstName = "Luke";
+console.log(firstName);
+console.log(1 + 1);
