f64fd4b012b0e9807b11d5b637cf3b04b2cfa374
sixLUKEY <marcoslukerojo@gmail.com>
2023-11-24 08:51:57 +0200

    star exercise complete

--- js-basics/Exercises/stars.js
+++ js-basics/Exercises/stars.js
@@ -1,7 +1,11 @@
 showStars( 5 );
 
 function showStars( rows ){
-    for ( let i = 0; i <= rows; i++ ){
-        console.log( '*' )
+    for ( let row = 1; row <= rows; row++ ){
+        let pattern = '';
+        for ( let i = 0; i < row; i++ ){
+            pattern += '*';
+        }
+        console.log( pattern );
     }
 }
